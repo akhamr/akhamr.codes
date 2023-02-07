968fc3b88abfac84f07c89b8fde9db2377346e30
Khamid Arrazaq <79617677+akhamr@users.noreply.github.com>
2023-02-07 06:47:17 +0000

    mantap

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 export default function Home() {
     return (
         <div className="flex flex-col justify-center items-center">
-            <p className="text-5xl font-medium">Coming Soon!</p>
-            <p className="text-5xl font-semibold">Coming Soon!</p>
-            <p className="text-5xl font-bold">Coming Soon!</p>
-            <p className="text-5xl font-extrabold">Coming Soon!</p>
+            <p className="text-5xl tracking-tight font-medium">Coming Soon!</p>
+            <p className="text-5xl tracking-tight font-semibold">
+                Coming Soon!
+            </p>
+            <p className="text-5xl tracking-tight font-bold">Coming Soon!</p>
+            <p className="text-5xl tracking-tight font-extrabold">
+                Coming Soon!
+            </p>
         </div>
     );
 }
