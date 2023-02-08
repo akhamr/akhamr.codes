62a6e6a464facc22a8568e5371d68d14f9713970
Khamid Arrazaq <saikonomia@gmail.com>
2023-02-08 21:58:09 +0700

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,111 @@
 import { getPostBySlug, getSlug } from '@/hooks/PostLib';
 import Link from 'next/link';
+import Image, { ImageProps } from 'next/image';
 import { MDXRemote } from 'next-mdx-remote/rsc';
-import { MdxComponent } from '@/styles/mdx';
 const day = require('dayjs');
+
+interface Props {
+    children?: React.ReactNode;
+}
+
+const CustomImage = ({ alt, ...props }: ImageProps) => {
+    return (
+        <div className="w-full text-center mt-2 mb-4">
+            <Image
+                {...props}
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="w-full h-auto"
+                alt={alt}
+            />
+            <p className="text-sm italic">{alt}</p>
+        </div>
+    );
+};
+
+const toValidSlug = (baseString: string): string => {
+    return baseString.replace(new RegExp(` `, `g`), `-`).toLowerCase();
+};
+
+const MdxComponent = () => {
+    return {
+        Img: CustomImage,
+        h1: ({ children }: Props) => {
+            return (
+                <h1
+                    id={toValidSlug(children as string)}
+                    className="scroll-margin-nav"
+                >
+                    <Link href={`#${toValidSlug(children as string)}`}>
+                        {children}
+                    </Link>
+                </h1>
+            );
+        },
+        h2: ({ children }: Props) => {
+            return (
+                <h2
+                    id={toValidSlug(children as string)}
+                    className="scroll-margin-nav"
+                >
+                    <Link href={`#${toValidSlug(children as string)}`}>
+                        {children}
+                    </Link>
+                </h2>
+            );
+        },
+        h3: ({ children }: Props) => {
+            return (
+                <h3
+                    id={toValidSlug(children as string)}
+                    className="scroll-margin-nav"
+                >
+                    <Link href={`#${toValidSlug(children as string)}`}>
+                        {children}
+                    </Link>
+                </h3>
+            );
+        },
+        h4: ({ children }: Props) => {
+            return (
+                <h4
+                    id={toValidSlug(children as string)}
+                    className="scroll-margin-nav"
+                >
+                    <Link href={`#${toValidSlug(children as string)}`}>
+                        {children}
+                    </Link>
+                </h4>
+            );
+        },
+        h5: ({ children }: Props) => {
+            return (
+                <h5
+                    id={toValidSlug(children as string)}
+                    className="scroll-margin-nav"
+                >
+                    <Link href={`#${toValidSlug(children as string)}`}>
+                        {children}
+                    </Link>
+                </h5>
+            );
+        },
+        h6: ({ children }: Props) => {
+            return (
+                <h6
+                    id={toValidSlug(children as string)}
+                    className="scroll-margin-nav"
+                >
+                    <Link href={`#${toValidSlug(children as string)}`}>
+                        {children}
+                    </Link>
+                </h6>
+            );
+        },
+    };
+}
+
 
 export default async function Post() {
     const slug = await getSlug();
