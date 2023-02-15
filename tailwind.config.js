0cd055ca487bb657414bab5b3f60647fe8401cb7
Khamid Arrazaq <saikonomia@gmail.com>
2023-02-15 07:13:58 +0000

    asu

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
                                     textDecoration: 'underline',
                                 },
                             },
+                            scrollMarginTop: '84px',
                         },
                     },
                 },
